756c072053351c651472483cb9f9477478c6ce4e
dirzhov <dirzhov@gmail.com>
2011-10-20 12:39:34 +0300

    update index.html

--- update-directory-index.bat
+++ update-directory-index.bat
@@ -2,5 +2,6 @@
 set REPO_DIR = ".\repo"
 for /d /r %REPO_DIR% %%d in (*) do (
 echo ^<html^>^<body^>^<h1^>Directory listing^</h1^>^<hr/^>^<pre^>
-dir /B %%d | findstr -v "^\./$" | findstr -v "^index\.html$" | gawk '{ printf "<a href=\"%%s\">%%s</a><br/>\n",$0,$0 }'
+dir /B %%d | findstr -v "^\./$" | findstr -v "^index\.html$" | gawk '{ printf "<a href=\"%%s\">%%s</a>",$0,$0 }'
+echo ^</body^>^</html^>
 ) > %%d/index.html
